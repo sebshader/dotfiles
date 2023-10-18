f7f6c96c97c36a262355b8aeabadd681497e2b3a
Sebastian Shader <sebfumaster@aol.com>
2023-10-17 22:36:35 -0700

    set term colors right for lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,6 +1,15 @@
 local custom_16color = require'lualine.themes.16color'
 
+custom_16color.terminal = {
+    a = {bg = 51}
+}
+
 custom_16color.normal.a.bg = 18
+custom_16color.normal.a.fg = 255
+custom_16color.insert.a.fg = 255
+custom_16color.visual.a.fg = 255
+custom_16color.replace.a.fg = 255
+custom_16color.inactive.a.fg = 255
 
 require('lualine').setup {
   options = {
