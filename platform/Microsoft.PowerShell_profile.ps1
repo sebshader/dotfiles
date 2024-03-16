414a2f346fd2526387a7de39032b027394a6a8fa
Sebastian Shader <sebfumaster@aol.com>
2024-03-16 14:42:01 -0500

    add powershell profile

---
+++ platform/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,47 @@
+
+# Shows navigable menu of all options when hitting Tab
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+#(& opam env) -split '\r?\n' | ForEach-Object { Invoke-Expression $_ }
+
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+
+Set-PsReadLineOption -EditMode Vi
+$j_timer = New-Object System.Diagnostics.Stopwatch
+
+Set-PSReadLineKeyHandler -Key j -ViMode Insert -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("j")
+    $j_timer.Restart()
+}
+
+Set-PSReadLineKeyHandler -Key k -ViMode Insert -ScriptBlock {
+    if (!$j_timer.IsRunning -or $j_timer.ElapsedMilliseconds -gt 500) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("k")
+    } else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::Delete($cursor, 1)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor-1)
+    }
+}
+
+$oldprompt = $function:prompt
+function prompt {
+    & $oldprompt
+    # things like vim/nvim can mess the cursor up
+    Write-Host -NoNewLine "`e[5 q"
+}
+
+Set-Alias -Name vim nvim
