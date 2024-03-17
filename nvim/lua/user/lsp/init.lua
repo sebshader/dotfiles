3dec788aecd49a7906c7ba2ccaedaf182ead2f3b
Sebastian Shader <sebfumaster@aol.com>
2024-03-17 05:04:32 -0500

    more width tweaks

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -28,16 +28,14 @@ vim.diagnostic.config(config)
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
-    width = 60,
-    wrap_at = 100,
-    max_width = 100,
+    wrap_at = 80,
+    max_width = 80
 })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
-    width = 60,
-    wrap_at = 100,
-    max_width = 100,
+    wrap_at = 80,
+    max_width = 80
 })
 
 local function lsp_highlight_document(client)
