f0252f2b8fc107f3ea1c651acee2fc3d35e76479
Sebastian Shader <sebfumaster@aol.com>
2024-03-17 05:49:14 -0500

    use powershell for nvim terminal mode

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -16,3 +16,15 @@ vim.opt.list = true
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case -j 1"
 vim.opt.grepformat:append("%f:%l:%c:%m")
 --vim.cmd.syntax("off")
+
+if(vim.fn.has('win32')) then
+    vim.opt.shell = (vim.fn.executable('pwsh') > 0) and 'pwsh' or 'powershell'
+    vim.opt.shellcmdflag = table.concat({'-NoLogo -ExecutionPolicy RemoteSigned ',
+        '-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text',
+        '.UTF8Encoding]::new();$PSDefaultParameterValues[\'\'Out-File:Encoding\'\'',
+        ']=\'\'utf8\'\';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'}, '')
+    vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    vim.opt.shellredir = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+    vim.opt.shellquote = ''
+    vim.opt.shellxquote = ''
+end
