0e8ef70b15268d5b8982490b8af21c2bd5680955
Sebastian Shader <sebfumaster@aol.com>
2024-03-24 00:16:47 -0500

    add delete buffer mappings in telescope

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,3 +1,17 @@
+require("telescope").setup {
+    pickers = {
+        buffers = {
+            mappings = {
+                i = {
+                    ["<c-d>"] = "delete_buffer"
+                },
+                n = {
+                    ["d"] = "delete_buffer"
+                }
+            }
+        }
+    }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files)
 vim.keymap.set('n', '<leader>gf', builtin.git_files)
