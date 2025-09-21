3674d7d42305fc21b12af22737422e904eec872b
Sebastian Shader <sebfumaster@aol.com>
2025-09-21 16:27:01 -0500

    update lazy and lsp config

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,23 +1,23 @@
 require('mason').setup()
 --require('mason-lspconfig').setup()
 
-local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 local config = {
     -- disable virtual text
     -- virtual_text = false,
     -- show signs
     signs = {
-        active = signs,
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        },
+        linehl = {
+            [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+        },
+        numhl = {
+            [vim.diagnostic.severity.WARN] = 'WarningMsg',
+        },
     },
     update_in_insert = true,
     underline = true,
